2707014c08f09a1244c30889e9597ae87375aa3c
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-14 22:21:45 -0400

    Strings complete

---
+++ strings.js
@@ -0,0 +1,17 @@
+var characterName = "Doug";
+characterName = "Lucy";
+var age = 26;
+
+let message = `Hello, my name is ${characterName}, and my age is ${age}`;
+
+let length = characterName.length;
+let upperName = characterName.toUpperCase();
+let lowerName = characterName.toLowerCase();
+
+// substrings
+
+let zen = characterName.slice(0, 3);
+
+let levelNum = 1
+
+let levelStartMessage = `Level ${levelNum}`
--- variables.js
+++ variables.js
@@ -4,3 +4,16 @@ currentHealth = maxHealth;
 
 let isGameOver = false;
 isGameOver = true;
+
+// const let var
+// var is a variable
+// let is scope based variable
+// const variable who cannot be changed
+
+if (true) {
+  var isGameOver = false;
+  let isNotGameOver = true;
+}
+
+console.log(isGameOver) // undefined - no code break
+console.log(isNotGameOver) // reference error - breaks code
